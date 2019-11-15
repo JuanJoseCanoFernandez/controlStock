1c5ff8bb8cce6124cad1b9887c1743370b5842f6
carlosjose1267 <36506011+carlosjose1267@users.noreply.github.com>
2019-11-15 09:51:16 +0100

    Add files via upload

---
+++ insert_actualizados.sql
@@ -0,0 +1,130 @@
+-- MOVILES --
+-- 1 -- 
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Huawei','P20_Lite','230');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Xaomi','Redmi_Note_6_Pro','201');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Realme_X2','Pearl_blue','165');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Samsung','Galaxy_A50','145');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Cubot','King_Kong','132');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Meizu','M3','168');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Alcatel','Bluish_black','200');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','ZTE','Blade_L8','110');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','Apple_iphone','Gris_Espacial','150');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Movil','OnePlus','Mirror_Black','130');
+
+-- ORDENADORES --
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','HP','Slimline','700');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','PcCom','Gold_cylon_pro','600');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','Lenovo','V520S','800');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','Medion_Akoya','P66033','1000');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','HP','OMEN8751010NS','900');
+
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','MSI','Trident39sh','560');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','HP_Pavilion','GD6900038NS','30');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','Dell_Vostro','3470','460');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','MSI','Infinite9sc877','750');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Ordenador','Acer','AspireXC330','850');
+
+
+-- TABLETS --
+
+-- 1 --
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Huawei','MediaPadT5','225');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Leotec','SupernovaPlus','200');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Woxtex','X100','206');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Samsung','Galaxy_TAB_A','245');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Lenovo','TAB_M10','197');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Apple','Ipad_Silver','185');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','SPC','Gravity_Max','195');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Brigmton','BTPC1025','230');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','SPC','Laika_tablet_7','210');
+
+INSERT INTO productos
+(tipo, marca, modelo, precio)
+VALUES('Tablet','Tailus','Zaphyr8003W','300');
+
