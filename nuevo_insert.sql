bb4a07fd581bd73c9eb1cec61be3bb7ca8f29c0c
carlosjose1267 <36506011+carlosjose1267@users.noreply.github.com>
2019-11-15 09:10:17 +0100

    Add files via upload

---
+++ nuevo_insert.sql
@@ -0,0 +1,130 @@
+-- MOVILES --
+-- 1 -- 
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Huawei','P20_Lite','230');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Xaomi','Redmi_Note_6_Pro','201');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Realme_X2','Pearl_blue','165');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Samsung','Galaxy_A50','145');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Cubot','King_Kong','132');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Meizu','M3','168');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Alcatel','Bluish_black','200');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','ZTE','Blade_L8','110');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','Apple_iphone','Gris_Espacial','150');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Moviles','OnePlus','Mirror_Black','130');
+
+-- ORDENADORES --
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','HP','Slimline','700');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','PcCom','Gold_cylon_pro','600');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','Lenovo','V520S','800');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','Medion_Akoya','P66033','1000');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','HP','OMEN8751010NS','900');
+
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','MSI','Trident39sh','560');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','HP_Pavilion','GD6900038NS','30');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','Dell_Vostro','3470','460');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','MSI','Infinite9sc877','750');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Ordenadores','Acer','AspireXC330','850');
+
+
+-- TABLETS --
+
+-- 1 --
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Huawei','MediaPadT5','225');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Leotec','SupernovaPlus','200');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Woxtex','X100','206');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Samsung','Galaxy_TAB_A','245');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Lenovo','TAB_M10','197');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Apple','Ipad_Silver','185');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','SPC','Gravity_Max','195');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Brigmton','BTPC1025','230');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','SPC','Laika_tablet_7','210');
+
+INSERT INTO `tienda`.`productos`
+(tipo, marca, modelo, precio)
+VALUES('Tablets','Tailus','Zaphyr8003W','300');
+
