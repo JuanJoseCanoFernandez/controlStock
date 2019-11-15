0e60d426f6901e4be8f163311dd195ddc0a756d1
Luis Alfonso <alfonso.fernandezbarrado@hotmail.com>
2019-11-15 09:36:16 +0100

    actualizacion php tras actualizacion BD

--- producto.php
+++ producto.php
@@ -1,20 +1,20 @@
 <?php
-abstract class Producto{
+class Producto{
 
-	protected $tipo;
-	protected $marca;
-	protected $modelo;
-	protected $precio;
-	protected $cantidad=0;
-	protected $idProducto;
+	private $tipo;
+	private $marca;
+	private $modelo;
+	private $precio;
+	private $cantidad=0;
+	private $idProducto;
 
-	public function __construct($tipo,$marca,$modelo,$precio,$idProducto)
+	public function __construct($tipo,$marca,$modelo,$precio)
 	{
 		$this->tipo=$tipo;
 		$this->marca=$marca;
 		$this->modelo=$modelo;
 		$this->precio=$precio;
-		$this->idProducto=$idProducto;
+		
 	}
 }
 ?>
