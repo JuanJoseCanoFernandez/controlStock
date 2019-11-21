d99fa91b33ff0c8701d2a2d705b1d367460cfdfc
snoflafernandez <43990310+snoflafernandez@users.noreply.github.com>
2019-11-21 10:44:43 +0100

    Actualizacion tienda

--- insert_actualizados.sql
+++ insert_actualizados.sql
@@ -1,130 +1,133 @@
+-- TIENDA --
+insert into tienda (nombre,email) values ('prueba','TU CORREO');
+
 -- MOVILES --
 -- 1 -- 
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Huawei','P20_Lite','230');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Huawei','P20_Lite','230',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Xaomi','Redmi_Note_6_Pro','201');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Xaomi','Redmi_Note_6_Pro','201',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Realme_X2','Pearl_blue','165');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Realme_X2','Pearl_blue','165',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Samsung','Galaxy_A50','145');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Samsung','Galaxy_A50','145',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Cubot','King_Kong','132');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Cubot','King_Kong','132',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Meizu','M3','168');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Meizu','M3','168',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Alcatel','Bluish_black','200');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Alcatel','Bluish_black','200',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','ZTE','Blade_L8','110');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','ZTE','Blade_L8','110',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','Apple_iphone','Gris_Espacial','150');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','Apple_iphone','Gris_Espacial','150',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Movil','OnePlus','Mirror_Black','130');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Movil','OnePlus','Mirror_Black','130',1);
 
 -- ORDENADORES --
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','HP','Slimline','700');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','HP','Slimline','700',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','PcCom','Gold_cylon_pro','600');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','PcCom','Gold_cylon_pro','600',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','Lenovo','V520S','800');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','Lenovo','V520S','800',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','Medion_Akoya','P66033','1000');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','Medion_Akoya','P66033','1000',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','HP','OMEN8751010NS','900');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','HP','OMEN8751010NS','900',1);
 
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','MSI','Trident39sh','560');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','MSI','Trident39sh','560',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','HP_Pavilion','GD6900038NS','30');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','HP_Pavilion','GD6900038NS','30',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','Dell_Vostro','3470','460');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','Dell_Vostro','3470','460',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','MSI','Infinite9sc877','750');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','MSI','Infinite9sc877','750',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Ordenador','Acer','AspireXC330','850');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Ordenador','Acer','AspireXC330','850',1);
 
 
 -- TABLETS --
 
 -- 1 --
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Huawei','MediaPadT5','225');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Huawei','MediaPadT5','225',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Leotec','SupernovaPlus','200');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Leotec','SupernovaPlus','200',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Woxtex','X100','206');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Woxtex','X100','206',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Samsung','Galaxy_TAB_A','245');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Samsung','Galaxy_TAB_A','245',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Lenovo','TAB_M10','197');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Lenovo','TAB_M10','197',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Apple','Ipad_Silver','185');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Apple','Ipad_Silver','185',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','SPC','Gravity_Max','195');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','SPC','Gravity_Max','195',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Brigmton','BTPC1025','230');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Brigmton','BTPC1025','230',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','SPC','Laika_tablet_7','210');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','SPC','Laika_tablet_7','210',1);
 
 INSERT INTO productos
-(tipo, marca, modelo, precio)
-VALUES('Tablet','Tailus','Zaphyr8003W','300');
+(tipo, marca, modelo, precio, tienda_idTienda)
+VALUES('Tablet','Tailus','Zaphyr8003W','300',1);
 
