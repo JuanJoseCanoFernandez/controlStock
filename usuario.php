1b3699141bb30803d7f2b12cbd4007606e54dbb6
Luis Alfonso <alfonso.fernandezbarrado@hotmail.com>
2019-11-25 14:43:20 +0100

    actualizacion archivos 25-11-2019

--- usuario.php
+++ usuario.php
@@ -2,16 +2,27 @@
 class Usuario{
 	
 	private $nombre;
-	private $contraseña;
 	private $interes;
 	private $emailUsuario;
 
-	public function __construct($nombre,$contraseña,$interes,$email)
+	public function __construct($nombre,$interes,$email)
 	{
 		$this->nombre=$nombre;
-		$this->contraseña=$contraseña;
 		$this->interes=$interes;
 		$this->emailUsuario=$email;
+	}
+	
+	public function nuevoUsuario()
+	{
+		$instance = Conexion::getInstance();
+        $conn = $instance->getConnection();
+        $nombre = $_POST["nombre"];
+        $interes = $_POST["interes"];
+        $email = $_POST["email"];
+        $instance = "INSERT INTO usuarios (nusuario,email,interes) values ('$nombre','$email','$interes')";
+        $result = $conn->prepare($instance);
+        $result->execute();
+        $datos=$result->fetchAll();
 	}
 }
 ?>
